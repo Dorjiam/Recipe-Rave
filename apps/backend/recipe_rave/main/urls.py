478eb88d0f8f3ec1d9f94fb4cb8574d63f7720ee
Dorjiam <DorjiamNotKnown@gmail.com>
2024-04-06 22:56:14 +0330

    mid commit

--- apps/backend/recipe_rave/main/urls.py
+++ apps/backend/recipe_rave/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("signup/", views.sign_up, name="sign_up"),
+    path("user/<str:name>", views.user, name="user"),
 ]
